a6b4963cd0e6adffe60079e834bc7786382d0569
marioraulgz <m.raul.guzmangtz@@gmail.com>
2021-06-23 00:38:37 -0500

    Totalmente funcional

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from os import sep
 import streamlit as st
 import spotipy as spoti
 import pandas as pd
@@ -6,12 +7,24 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import pickle
 import sklearn
 import time
+import SessionState
+
+# Definir Session State
+session_state = SessionState.get(
+    dic_ids={},
+    dic_explicit={},
+    dic_artists={},
+    list_song_names=[],
+    number_results=0,
+    valid_search=False,
+)
 
 # Paraemtros Spotipy
 CID = "1c5eb54d90c1432e9cfdd15797a78f94"
 SECRET = "e55cf83e20304dc6bdc43760c2e2791c"
 MODEL_FILENAME = "./tree.sav"
 n_results = 6
+number_results = 0
 
 ## Parametros modelo
 tree_model = pickle.load(open(MODEL_FILENAME, "rb"))
@@ -39,24 +52,28 @@ sp = spoti.Spotify(client_credentials_manager=client_credentials_manager)
 
 
 def search_results_fetch(query, n_results):
-    search_results = sp.search(q="", limit=n_results, type="track")
+    search_results = sp.search(q=query, limit=n_results, type="track")
     dic_ids = {}
     dic_explicit = {}
     dic_artists = {}
-    list_song_names = []
-    for i in range(0, n_results):
+    number_results = max(n_results, len(search_results["tracks"]["items"]))
+    list_song_names = [0 for i in range(number_results)]
+    for i in range(number_results):
+        print(i)
         artist_i = search_results["tracks"]["items"][i]["artists"]
-        list_song_names[i] = search_results["tracks"]["items"][0]["name"]
-        dic_ids[i] = search_results["tracks"]["items"][0]["id"]
-        dic_explicit[i] = search_results["tracks"]["items"][0]["explicit"]
+        list_song_names[i] = search_results["tracks"]["items"][i]["name"]
+        dic_ids[i] = search_results["tracks"]["items"][i]["id"]
+        dic_explicit[i] = search_results["tracks"]["items"][i]["explicit"]
         dic_artists[i] = [artist["name"] for artist in artist_i]
-    return dic_ids, dic_explicit, dic_artists, list_song_names
+    return dic_ids, dic_explicit, dic_artists, list_song_names, number_results
 
 
 def show_search_results(dic_artists, list_song_names, dic_explicit):
     for i in range(len(list_song_names)):
         st.write(f"{i+1}. {list_song_names[i]}")
-        st.write(dic_artists[i])
+        st.write(dic_artists[i], sep=",")
+        if dic_explicit[i]:
+            st.warning("Explicit")
 
 
 def song_data_fetch(song_id, explicit):
@@ -68,65 +85,91 @@ def song_data_fetch(song_id, explicit):
         axis=1,
         inplace=True,
     )
+    df.rename(columns={"duration_ms": "track.duration_ms"}, inplace=True)
     final_df = df[columns_model]
 
     return final_df
 
 
-## Cargar modelo
-
 try:
     st.title("Spotify Streamlit")
+    add_selectbox = st.sidebar.write("Equipo 2 BEDU")
+    time.sleep(2.00)
     st.write(
         """
-    Conectados a Spotify
+    Conectados a Spotify :white_check_mark:
     """
     )
+    time.sleep(0.1)
+    st.write(
+        "Usa un modelo de Árbol de Decisión para predecir si una canción es popular o no"
+    )
+    with st.beta_expander("¿Árbol de Decisión?"):
+        st.write(
+            "Aprendizaje basado en árboles de decisión utiliza un árbol de decisión como un modelo predictivo que mapea observaciones sobre un artículo a conclusiones sobre el valor objetivo del artículo. Es uno de los enfoques de modelado predictivo utilizadas en estadísticas, minería de datos y aprendizaje automático. Los modelos de árbol, donde la variable de destino puede tomar un conjunto finito de valores se denominan árboles de clasificación. En estas estructuras de árbol, las hojas representan etiquetas de clase y las ramas representan las conjunciones de características que conducen a esas etiquetas de clase. Los árboles de decisión, donde la variable de destino puede tomar valores continuos (por lo general números reales) se llaman árboles de regresión. Los árboles de decisión se encuentran entre los algoritmos populares debido a su simplicidad.En análisis de decisión, un árbol de decisión se puede utilizar para representar visualmente y de forma explícita decisiones y toma de decisiones. En minería de datos, un árbol de decisión describe datos, pero no las decisiones; más bien el árbol de clasificación resultante puede ser un usado como entrada para la toma de decisiones."
+        )
 
-    ## Caja de texto en el lateral
+    ## Buscador de canciones
     st.subheader("Escribe una canción")
-    ## Boton
     with st.form(key="search_form"):
-        query = st.text_input(label="Busqueda_Cancion")
-        submit_button_query = st.form_submit_button(label="Submit_query")
+        query = st.text_input(label="Busqueda Cancion")
+        submit_button_query = st.form_submit_button(label="Buscar")
 
     if submit_button_query:
-        dic_ids, dic_explicit, dic_artists, list_song_names = search_results_fetch(
-            query, n_results
-        )
-        show_search_results(dic_artists, list_song_names, dic_explicit)
+        (
+            dic_ids_var,
+            dic_explicit_var,
+            dic_artists_var,
+            list_song_names_var,
+            number_results_var,
+        ) = search_results_fetch(query, n_results)
+        show_search_results(dic_artists_var, list_song_names_var, dic_explicit_var)
 
-    st.subheader("Selecciona cual canción buscabas")
+        session_state.dic_ids = dic_ids_var
+        session_state.dic_explicit = dic_explicit_var
+        session_state.dic_artists = dic_artists_var
+        session_state.list_song_names = list_song_names_var
+        session_state.number_results = number_results_var
+        session_state.valid_search = True
+        # Opciones del 1 al 6
+    if session_state.valid_search:
+        st.subheader("Selecciona cual canción buscabas")
+        with st.form(key="model_form"):
+            param_modelo = st.radio(
+                label="Numero de la cancion", options=range(1, n_results + 1)
+            )
+            submit_button_modelo = st.form_submit_button(label="Calcular")
 
-    # Opciones del 1 al 6
-    with st.form(key="model_form"):
-        param_modelo = st.radio(
-            label="Opciones_cancion", options=range(1, n_results + 1)
-        )
-        submit_button_modelo = st.form_submit_button(label="Submit_cancion")
+        if submit_button_modelo:
+            final_df = song_data_fetch(
+                session_state.dic_ids[param_modelo - 1],
+                session_state.dic_explicit[param_modelo - 1],
+            )
+            prediction = tree_model.predict(final_df)
 
-    if submit_button_query:
-        final_df = song_data_fetch(
-            dic_ids[int(param_modelo + 1)], dic_explicit[int(param_modelo + 1)],
-        )
-        prediction = tree_model.predict(final_df)
+            st.write("Empezando predicción")
+            time.sleep(1)
+            progress_bar = st.progress(0)
+            status_text = st.empty()
+            for i in range(100):
+                # Actualizamos la barra de progreso
+                progress_bar.progress(i + 1)
 
-        progress_bar = st.progress(0)
-        status_text = st.empty()
-        for i in range(100):
-            # Actualizamos la barra de progreso
-            progress_bar.progress(i + 1)
+                # Pretendamos que estamos gastando tiempo
+                time.sleep(0.02)
+            status_text.success("¡Listo!")
+            st.header("Resultados de la **Inteligencia Artificial**")
+            st.write(f"Para la canción {int(param_modelo)}")
 
-            # Pretendamos que estamos gastando tiempo
-            time.sleep(0.09)
-        status_text.text("¡Listo!")
-        st.header("Resultados de la **Inteligencia Artificial**")
-        st.write(f"Para la canción {int(param_modelo)+1}")
+            if prediction:
+                st.balloons()
+                st.write("La canción será exitosa!")
+            else:
+                st.write(":pensive: No te preocupes, las preferencias siempre cambian!")
 
-        if prediction:
-            st.balloons()
-        else:
-
+            submit_button_reiniciar = st.button("Limpiar")
+            if submit_button_reiniciar:
+                session_state.valid_search = False
     # Agarrar ID De esa opcion
 
     # Mostrar abajo que se esta calculando
